dfd6c0725f7d4afd4253e2a32c4d40e14cb5368f
Ahmed Soliman <Ahmedhsmsa@gmail.com>
2026-02-28 22:24:15 +0200

    Fix work orders page import

---
+++ app/(app)/finance/purchases/[id]/page.tsx
@@ -0,0 +1,6 @@
+// app/(app)/finance/purchases/[id]/page.tsx
+import PurchaseDetailsClientPage from "./PurchaseDetailsClientPage";
+
+export default function Page() {
+  return <PurchaseDetailsClientPage />;
+}
