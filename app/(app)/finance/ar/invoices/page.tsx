0070b5a1799d04b4747c296d1df634e4dc864da6
Ahmed Soliman <Ahmedhsmsa@gmail.com>
2026-03-04 01:14:15 +0200

    Fix AR invoices page module export

---
+++ app/(app)/finance/ar/invoices/page.tsx
@@ -0,0 +1,7 @@
+// app/(app)/finance/ar/invoices/page.tsx
+import React from "react";
+import ArInvoicesClientPage from "./InvoicesClientPage";
+
+export default function Page() {
+  return <ArInvoicesClientPage />;
+}
