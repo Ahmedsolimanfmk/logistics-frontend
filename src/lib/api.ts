e7031413c5de019fd85cf57ba93faae2925d2f82
Ahmed Soliman <Ahmedhsmsa@gmail.com>
2026-05-15 17:22:50 +0300

    fix circular dependency

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,20 +4,19 @@ export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
-// 🔥 أهم جزء
 api.interceptors.request.use((config) => {
-  try {
+  if (typeof window !== "undefined") {
     const token = localStorage.getItem("token");
+    const companyId = localStorage.getItem("company_id");
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
 
-    const companyId = localStorage.getItem("company_id");
     if (companyId) {
       config.headers["x-company-id"] = companyId;
     }
-  } catch {}
+  }
 
   return config;
 });
